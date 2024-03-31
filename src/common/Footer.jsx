444b35806c176934ccdda2eb392da99c398690d2
Samiul Fahad <samiul_fahad@yahoo.com>
2024-04-01 04:37:36 +0600

    theme updated

--- src/common/Footer.jsx
+++ src/common/Footer.jsx
@@ -7,6 +7,7 @@ const Footer = () => {
         <div className="text-center">
             <p className="font-bold text-white">City Public School, Durgapur</p>
             <p className="text-white">Durgapur Educational Foundation</p>
+            <a href="https://fb.com/samiulfahad123" target="blank" className="font-bold">Developed by <span className="italic underline">Samiul Fahad</span></a>
         </div>
     </div>
   )
